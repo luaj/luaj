d7e0aeaac55333a7150f6680685ea4fbe0e64907
Shu Lei <anonymous@example.com>
2007-11-29 22:59:09 +0000

    bug fix

--- src/debug/org/luaj/debug/net/j2se/DebugSupportImpl.java
+++ src/debug/org/luaj/debug/net/j2se/DebugSupportImpl.java
@@ -148,7 +148,7 @@ public class DebugSupportImpl extends DebugSupport {
                     new Thread(clientConnectionTask).start();
                 }
             }
-        } finally {
+        } catch (IOException e) {
             dispose();
         }
     }
