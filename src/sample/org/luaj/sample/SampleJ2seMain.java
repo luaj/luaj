b639a3ca3ec7a668ec2ea7efd2296dd54773f375
James Roseborough <james.roseborough@luaj.org>
2008-12-03 06:07:29 +0000

    Add sample startup code for j2se

---
+++ src/sample/org/luaj/sample/SampleJ2seMain.java
@@ -0,0 +1,16 @@
+package org.luaj.sample;
+import org.luaj.platform.*;
+import org.luaj.vm.*;
+
+public class SampleJ2seMain {
+
+	public static void main(String[] args) {
+		String script = (args.length>0? args[0]: "src/test/res/swingapp.lua");
+		Platform.setInstance( new J2sePlatform() );
+		LuaState vm = Platform.newLuaState();
+		org.luaj.compiler.LuaC.install();
+		vm.getglobal( "dofile" );
+		vm.pushstring( script );
+		vm.call( 1, 0 );
+	}
+}
