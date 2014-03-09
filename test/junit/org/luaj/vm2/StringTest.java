7b97573ac8002ac808dcf12720c5f5816b3c5205
James Roseborough <james.roseborough@luaj.org>
2014-03-09 15:33:44 +0000

    Fix balanced match for empty string (fixes issue #23)

--- test/junit/org/luaj/vm2/StringTest.java
+++ test/junit/org/luaj/vm2/StringTest.java
@@ -349,4 +349,33 @@ public class StringTest extends TestCase {
 		assertEquals(1, sub.indexOfAny(CdEFGHIJ));
 		assertEquals(-1, sub.indexOfAny(EFGHIJKL));
 	}
+	
+	public void testMatchShortPatterns() {
+		LuaValue[] args = { LuaString.valueOf("%bxy") };
+		LuaString _ = LuaString.valueOf("");
+
+		LuaString a = LuaString.valueOf("a");
+		LuaString ax = LuaString.valueOf("ax");
+		LuaString axb = LuaString.valueOf("axb");
+		LuaString axby = LuaString.valueOf("axby");
+		LuaString xbya = LuaString.valueOf("xbya");
+		LuaString bya = LuaString.valueOf("bya");
+		LuaString xby = LuaString.valueOf("xby");
+		LuaString axbya = LuaString.valueOf("axbya");
+		LuaValue nil = LuaValue.NIL;
+		
+		assertEquals(nil, _.invokemethod("match", args));
+		assertEquals(nil, a.invokemethod("match", args));
+		assertEquals(nil, ax.invokemethod("match", args));
+		assertEquals(nil, axb.invokemethod("match", args));
+		assertEquals(xby, axby.invokemethod("match", args));
+		assertEquals(xby, xbya.invokemethod("match", args));
+		assertEquals(nil, bya.invokemethod("match", args));
+		assertEquals(xby, xby.invokemethod("match", args));
+		assertEquals(xby, axbya.invokemethod("match", args));
+		assertEquals(xby, axbya.substring(0,4).invokemethod("match", args));
+		assertEquals(nil, axbya.substring(0,3).invokemethod("match", args));
+		assertEquals(xby, axbya.substring(1,5).invokemethod("match", args));
+		assertEquals(nil, axbya.substring(2,5).invokemethod("match", args));
+	}
 }
