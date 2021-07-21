66130964c6fc149ebcb1a0a74f0a17ef3e63656e
Enrico Horn <farmboy0+git@googlemail.com>
2021-07-21 23:59:42 +0200

    Convert mathlib.modf result to number

--- luaj-core/src/main/java/org/luaj/vm2/lib/MathLib.java
+++ luaj-core/src/main/java/org/luaj/vm2/lib/MathLib.java
@@ -300,7 +300,7 @@ public class MathLib extends TwoArgFunction {
 			LuaValue n = args.arg1();
 			/* number is its own integer part, no fractional part */
 			if (n.islong())
-				return varargsOf(n, valueOf(0.0));
+				return varargsOf(n.tonumber(), valueOf(0.0));
 			double x = n.checkdouble();
 			/* integer part (rounds toward zero) */
 			double intPart = x > 0? Math.floor(x): Math.ceil(x);
